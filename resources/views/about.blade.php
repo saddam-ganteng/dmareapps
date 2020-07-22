5371c3bf2288c0e80c70845986e00b18bc3afb13
Saddam Dermawan <55724836+dermadn@users.noreply.github.com>
2020-07-22 11:37:41 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+</head>
+<body style=" background-color: #e5e5e5;">
+    <div class="jumbotron pt-4 pb-4">
+        <h1 class="display-6">Dmareapps</h1>
+        <hr class="my-3">
+        <nav class="navbar navbar-expand-lg ">          
+            <div class="collapse navbar-collapse " id="navbarNav">
+              <ul class="navbar-nav">
+                <li class="nav-item active">
+                  <a class="nav-link" href="about">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="experience">Experience </a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="portofolio">Portofolio </a>
+                </li>
+              </ul>
+            </div>
+        </nav>
+    </div>
+
+    <div class="container p-4" style="background-color: white">
+        <div class="row">
+        <div class="col-md-8">
+            <div class="posts">
+                <h2 class="title-post">About</h2>
+               
+                <div class="">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nisi veniam quibusdam amet, odit, odio consectetur ratione quod, cupiditate repellendus voluptatum. Laudantium tempora, neque quo ex aspernatur veritatis sequi incidunt.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nisi veniam quibusdam amet, odit, odio consectetur ratione quod, cupiditate repellendus voluptatum. Laudantium tempora, neque quo ex aspernatur veritatis sequi incidunt.</p>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officiis nisi veniam quibusdam amet, odit, odio consectetur ratione quod, cupiditate repellendus voluptatum. Laudantium tempora, neque quo ex aspernatur veritatis sequi incidunt.</p>
+                </div>
+            </div>
+         </div>
+         <div class="col-md-4 ">
+            <h3 class="widget-title">Kontak Saya</h3>
+            <ul>
+                <li><a href="#">Lorem Ipsum Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque nobis, error in numquam quas inventore maxime exercitationem ipsum optio, quidem eos sapiente cum dolorum, impedit ut rerum fugiat porro distinctio!</a></li>
+                <li><a href="#">Lorem Ipsum</a></li>
+                <li><a href="#">Lorem Ipsum</a></li>
+                <li><a href="#">Lorem Ipsum</a></li>
+                <li><a href="#">Lorem Ipsum</a></li>
+            </ul>
+        </div>
+        </div>
+    </div>
+
+    
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('kategori/{kategori}/{judul}', function ($judul, $kategori) {
+    return "Kategori : $kategori, Title : $judul";
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/experience', function () {
+    return view('experience');
+});
+
+Route::get('/portofolio', function () {
+    return view('portofolio');
+});
+
+
