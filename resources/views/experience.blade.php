03a5e2114534679cf9cdc7e2a02cf31cb91bc4b7
Saddam Dermawan <55724836+dermadn@users.noreply.github.com>
2020-07-22 16:33:40 +0700

    enter

--- resources/views/experience.blade.php
+++ resources/views/experience.blade.php
@@ -1,47 +1,81 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
+
 <body style=" background-color: #e5e5e5;">
     <div class="jumbotron pt-4 pb-4">
         <h1 class="display-6">Dmareapps</h1>
         <hr class="my-3">
-        <nav class="navbar navbar-expand-lg ">          
+        <nav class="navbar navbar-expand-lg ">
             <div class="collapse navbar-collapse " id="navbarNav">
-              <ul class="navbar-nav">
-                <li class="nav-item active">
-                  <a class="nav-link" href="about">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="experience">Experience </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="portofolio">Portofolio </a>
-                </li>
-              </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="about">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="experience">Experience </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="portofolio">Portofolio </a>
+                    </li>
+                </ul>
             </div>
         </nav>
     </div>
 
     <div class="container p-4 " style="background-color: white">
         <div class="col-md-18">
-            <article class="posts">
-                <h2 class="title-post">Experience</h2>
-                <div class="">
-                    <div class="progress ">
-                      <div class="progress-bar" role="progressbar" style="width: 25%;" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
+
+
+            <h2 class="title-post">Experience</h2>
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="progress m-4">
+                        <div class="progress-bar" role="progressbar" style="width: 89%;" aria-valuenow="89"
+                            aria-valuemin="0" aria-valuemax="100">HTML</div>
                     </div>
-                    
+
+                    <div class="progress m-4">
+                        <div class="progress-bar" role="progressbar" style="width: 60%;" aria-valuenow="25"
+                            aria-valuemin="0" aria-valuemax="100">CSS</div>
+                    </div>
+
+                    <div class="progress m-4">
+                        <div class="progress-bar" role="progressbar" style="width: 60%;" aria-valuenow="25"
+                            aria-valuemin="0" aria-valuemax="100">JavaScript</div>
+                    </div>
                 </div>
-            </article>
-         </div>
+                
+
+                <div class="col-md-6">
+                    <div class="progress m-4">
+                        <div class="progress-bar" role="progressbar" style="width: 73%;" aria-valuenow="25"
+                            aria-valuemin="0" aria-valuemax="100">Java</div>
+                    </div>
+
+                    <div class="progress m-4">
+                        <div class="progress-bar" role="progressbar" style="width: 83%;" aria-valuenow="25"
+                            aria-valuemin="0" aria-valuemax="100">PHP</div>
+                    </div>
+                    <div class="progress m-4">
+                        <div class="progress-bar" role="progressbar" style="width: 69%;" aria-valuenow="25"
+                            aria-valuemin="0" aria-valuemax="100">Photoshop</div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
     </div>
 
-    
+
 </body>
+
 </html>
